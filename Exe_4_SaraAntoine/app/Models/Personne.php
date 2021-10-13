44eb9eedecf8a98203902fa4e43300f4525e0944
Tomberryy <antoine.monnin@gmail.com>
2021-10-13 15:56:29 +0200

    Push models

--- Exe_4_SaraAntoine/app/Models/Personne.php
+++ Exe_4_SaraAntoine/app/Models/Personne.php
@@ -8,4 +8,28 @@ use Illuminate\Database\Eloquent\Model;
 class Personne extends Model
 {
     use HasFactory;
+
+    //Fillable
+    protected $fillable = ['nom', 'prenom', 'mail'];
+
+
+    //Liaison chef de projet
+    public function projets()
+    {
+        return $this->hasMany(Projet::class);
+    }
+
+
+    //Liaison auteur document
+    public function documents()
+    {
+        return $this->hasMany(Document::class);
+    }
+
+
+    //Liaison thérapeute
+    public function consultations()
+    {
+        return $this->hasMany(Consultation::class);
+    }
 }
